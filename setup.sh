57d940d9816c0ff014ab662bc0e1eea51137ea49
Nhat Dao <daoanhnhat1995@gmail.com>
2016-06-05 01:54:51 -0500

    base setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ CURRENTDIR="$PWD"
 
 
 #===========VIM SETUP ============*/
-echo '... Setting up Vim'
+echo '>>> Setting up Vim'
+ln -s $PWD/vim/vimrc ~/.vimrc 
+
+echo '>>> Tmux setting '
+ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf
+
+
 
 
 
